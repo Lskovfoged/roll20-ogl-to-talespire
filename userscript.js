d8cd83bad7924e386a27a0a41a80c24867ed7136
Lskovfoged <35615041+Lskovfoged@users.noreply.github.com>
2024-07-07 00:13:54 +0200

    Update userscript.js

--- userscript.js
+++ userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Talespire20
 // @namespace    https://drewers.dev
-// @version      0.3
+// @version      0.4
 // @description  Roll20 OGL Sheet Roller for Talespire
 // @author       Steven Drewers
 // @match        https://app.roll20.net/*
@@ -16,54 +16,56 @@
 
 window.jQuery360 = $.noConflict(true);
 
-$(document).ready(function(){
-    function sanitizeData(data){
+$(document).ready(function () {
+    function sanitizeData(data) {
         var san_roll = {
             name: "",
             die: "",
             count: "",
             mods: []
         };
-        // prepare data (currently json object)
+
         var roll;
         try {
             roll = JSON.parse(data);
         } catch (e) {
             console.error("Failed to parse roll data:", e);
-            return;
+            return null;
         }
 
-        var v = roll.rolls[0];
-        var current_roll = v.vre.rolls;
+        if (!roll || !roll.rolls || !roll.rolls[0] || !roll.rolls[0].vre) {
+            console.error("Invalid roll data structure:", roll);
+            return null;
+        }
 
-        $.each(current_roll, function(i, v) {
-            if (this.type == "R"){
+        var current_roll = roll.rolls[0].vre.rolls;
+        $.each(current_roll, function () {
+            if (this.type === "R") {
                 san_roll.die = this.sides;
                 san_roll.count = this.dice;
-            }
-            else if (this.type == "M"){
+            } else if (this.type === "M") {
                 san_roll.mods.push(this.expr);
             }
         });
-        // make talespire url
-        makeTalespireLink(san_roll);
+
+        return san_roll;
     }
 
-    function makeTalespireLink(data){
-        var modifiers = "";
-        $.each(data.mods, function(i, v) {
-            modifiers = modifiers.concat(v);
-        });
+    function makeTalespireLink(data) {
+        if (!data) {
+            return;
+        }
 
-        console.log(modifiers);
-        var sum = eval(modifiers);
-        var talespire_string = "";
-        if (sum >= 0){
-            talespire_string = "talespire://dice/" + data.count + "d" + data.die + "+" + sum;
+        var modifiers = data.mods.join('');
+        var sum;
+        try {
+            sum = new Function("return " + modifiers)();
+        } catch (e) {
+            console.error("Failed to evaluate modifiers:", e);
+            return;
         }
-        else {
-            talespire_string = "talespire://dice/" + data.count + "d" + data.die + "-" + sum;
-        }
+
+        var talespire_string = "talespire://dice/" + data.count + "d" + data.die + (sum >= 0 ? "+" + sum : sum);
         console.log(talespire_string);
         location.href = talespire_string;
     }
@@ -72,17 +74,17 @@ $(document).ready(function(){
         send = window.XMLHttpRequest.prototype.send;
 
     function openReplacement(method, url, async, user, password) {
-        this._url = url; // store the URL
+        this._url = url;
         return open.apply(this, arguments);
     }
 
     function sendReplacement(data) {
-        // Only intercept requests related to specific Roll20 roll actions
-        if (this._url.includes("roll")) {
+        if (this._url.includes("/rolls/")) {
             try {
                 var parsedData = JSON.parse(data);
                 if (parsedData && parsedData.rolls) {
-                    sanitizeData(data);
+                    var sanitizedData = sanitizeData(data);
+                    makeTalespireLink(sanitizedData);
                 }
             } catch (e) {
                 console.error("Failed to process roll data:", e);
@@ -107,11 +109,10 @@ $(document).ready(function(){
     window.XMLHttpRequest.prototype.send = sendReplacement;
 });
 
-(function() {
+(function () {
     'use strict';
 
-    // Your code here...
-    $("button[type='roll']").click(function(event){
+    $(document).on("click", "button[type='roll']", function (event) {
         event.preventDefault();
         $(this).addClass("ivebeenclicked");
     });

a51f90d694bf7deb93c835ec779998d91b681ce3
Lskovfoged <35615041+Lskovfoged@users.noreply.github.com>
2024-07-06 23:46:02 +0200

    Update userscript.js

--- userscript.js
+++ userscript.js
@@ -1,13 +1,15 @@
 // ==UserScript==
 // @name         Talespire20
 // @namespace    https://drewers.dev
-// @version      0.1
+// @version      0.3
 // @description  Roll20 OGL Sheet Roller for Talespire
 // @author       Steven Drewers
 // @match        https://app.roll20.net/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js
 // @grant        none
+// @updateURL    https://raw.githubusercontent.com/siebenzwiebel/roll20-ogl-to-talespire/main/userscript.js
+// @downloadURL  https://raw.githubusercontent.com/siebenzwiebel/roll20-ogl-to-talespire/main/userscript.js
 // ==/UserScript==
 
 /* global $ */
@@ -15,24 +17,26 @@
 window.jQuery360 = $.noConflict(true);
 
 $(document).ready(function(){
-
-
     function sanitizeData(data){
-
         var san_roll = {
             name: "",
             die: "",
             count: "",
             mods: []
-        }
-    // prepare data (currently json object)
+        };
+        // prepare data (currently json object)
         var roll;
-        roll = JSON.parse(data);
+        try {
+            roll = JSON.parse(data);
+        } catch (e) {
+            console.error("Failed to parse roll data:", e);
+            return;
+        }
 
         var v = roll.rolls[0];
         var current_roll = v.vre.rolls;
 
-        $.each(current_roll, function( i, v ) {
+        $.each(current_roll, function(i, v) {
             if (this.type == "R"){
                 san_roll.die = this.sides;
                 san_roll.count = this.dice;
@@ -42,24 +46,16 @@ $(document).ready(function(){
             }
         });
         // make talespire url
-
         makeTalespireLink(san_roll);
-
     }
 
     function makeTalespireLink(data){
-
-        //console.log($("button.ivebeenclicked").attr("name"));
-        //data.name = $("button.ivebeenclicked").attr("name");
-        //console.log(data);
-        //$("button.ivebeenclicked").removeClass("ivebeenclicked");
         var modifiers = "";
-
-        $.each(data.mods, function( i, v ) {
+        $.each(data.mods, function(i, v) {
             modifiers = modifiers.concat(v);
         });
 
-        console.log(modifiers)
+        console.log(modifiers);
         var sum = eval(modifiers);
         var talespire_string = "";
         if (sum >= 0){
@@ -67,38 +63,33 @@ $(document).ready(function(){
         }
         else {
             talespire_string = "talespire://dice/" + data.count + "d" + data.die + "-" + sum;
-
         }
         console.log(talespire_string);
-        location.href=talespire_string;
-
-
+        location.href = talespire_string;
     }
 
     var open = window.XMLHttpRequest.prototype.open,
-    send = window.XMLHttpRequest.prototype.send,
-    onReadyStateChange;
+        send = window.XMLHttpRequest.prototype.send;
 
     function openReplacement(method, url, async, user, password) {
-        var syncMode = async !== false ? 'async' : 'sync';
-        console.warn(
-            'Preparing ' +
-            syncMode +
-            ' HTTP request : ' +
-            method +
-            ' ' +
-            url
-        );
+        this._url = url; // store the URL
         return open.apply(this, arguments);
     }
 
     function sendReplacement(data) {
-        console.warn('Sending HTTP request data : ', data);
+        // Only intercept requests related to specific Roll20 roll actions
+        if (this._url.includes("roll")) {
+            try {
+                var parsedData = JSON.parse(data);
+                if (parsedData && parsedData.rolls) {
+                    sanitizeData(data);
+                }
+            } catch (e) {
+                console.error("Failed to process roll data:", e);
+            }
+        }
 
-        //DO SOMETHING WITH DATA
-        sanitizeData(data);
-
-        if(this.onreadystatechange) {
+        if (this.onreadystatechange) {
             this._onreadystatechange = this.onreadystatechange;
         }
         this.onreadystatechange = onReadyStateChangeReplacement;
@@ -107,31 +98,21 @@ $(document).ready(function(){
     }
 
     function onReadyStateChangeReplacement() {
-        console.warn('HTTP request ready state changed : ' + this.readyState);
-        if(this._onreadystatechange) {
+        if (this._onreadystatechange) {
             return this._onreadystatechange.apply(this, arguments);
+        }
     }
 
-
-
-}
-
-window.XMLHttpRequest.prototype.open = openReplacement;
-window.XMLHttpRequest.prototype.send = sendReplacement;
+    window.XMLHttpRequest.prototype.open = openReplacement;
+    window.XMLHttpRequest.prototype.send = sendReplacement;
 });
-
-
-
 
 (function() {
     'use strict';
 
     // Your code here...
-    $("button[type='roll']").click(function(){
+    $("button[type='roll']").click(function(event){
         event.preventDefault();
         $(this).addClass("ivebeenclicked");
     });
-  
-  
-
 })();
